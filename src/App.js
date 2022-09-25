44ab2cb428dfdfaba2423cebd399428e2bb7d58a
QianWu <1727670763@qq.com>
2022-09-25 16:35:29 +0800

    9.25-导航/路由模块初步

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
 import "./App.css";
-import Hello from "./components/Hello";
-
+import { useState } from "react";
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Menu } from "antd";
 import { NavLink, Routes, Route } from "react-router-dom";
 import Charts from "./components/charts";
-import axios from "axios";
 
 function App() {
+  const items = [
+    {
+      label: <NavLink to="/charts">首页</NavLink>,
+      key: "home-page",
+      icon: <MailOutlined />,
+    },
+    {
+      label: "全局数据检索",
+      key: "global-search",
+      icon: <AppstoreOutlined />,
+    },
+    {
+      label: "重点数据检索",
+      key: "key-point-search",
+      icon: <SettingOutlined />,
+    },
+    {
+      label: "数据预测分析",
+      key: "analysis",
+      icon: <SettingOutlined />,
+    },
+  ];
+
+  const [current, setCurrent] = useState("mail");
+
+  const onClick = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+
   return (
     <div className="App">
-      <NavLink to="/hello">Hello</NavLink>
-      <NavLink to="/charts">图表</NavLink>
-
       <Routes>
-        <Route path="/hello" element={<Hello />}></Route>
         <Route path="/charts" element={<Charts />}></Route>
       </Routes>
+      <Menu
+        onClick={onClick}
+        selectedKeys={[current]}
+        mode="horizontal"
+        items={items}
+      />
+      ;
     </div>
   );
 }
